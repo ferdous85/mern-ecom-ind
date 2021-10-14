6f05ad1de881ab8455254333fc620bbf8897d0a9
ferdous85 <ferdous85@gmail.com>
2021-10-14 13:12:53 +0600

    header and footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import './App.css'
-import Header from './component/layout/Header'
+import Header from './component/layout/Header/Header'
+import Footer from './component/layout/Footer/Footer'
 import { BrowserRouter as Router } from 'react-router-dom'
+import WebFont from "webfontloader";
+import { useEffect } from 'react';
 
 
 function App() {
+  useEffect(()=>{
+    WebFont.load({
+      google:{
+        families:["Roboto", 'Dorid Sans', "Chilanka"]
+      }
+    })
+  },[])
   return (
     <Router>
     <Header />
+    <Footer />
     </Router>
   );
 }

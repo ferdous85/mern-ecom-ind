ba79a6d5577e4cf6c7e829f660f09d2aba663d8d
ferdous85 <ferdous85@gmail.com>
2021-10-14 06:32:14 +0600

    order

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const { newOrder, getSingleOrder, myOrder, getAllOrders, updateOrder, deleteOrder } = require('../controllers/orderController')
+const { isAuthenticatedUser, authorizedRoles } = require('../middleware/auth')
+
+router.route('/order/new').post(isAuthenticatedUser, newOrder)
+
+router.route('/order/:id').get(isAuthenticatedUser,  getSingleOrder)
+
+router.route('/orders/me').get(isAuthenticatedUser, myOrder)
+
+router.route('/admin/orders').get(isAuthenticatedUser, authorizedRoles('admin'), getAllOrders)
+
+router.route('/admin/order/:id').put(isAuthenticatedUser, authorizedRoles('admin'), updateOrder)
+    .delete(isAuthenticatedUser, authorizedRoles('admin'), deleteOrder)
+
+
+
+
+
+module.exports = router

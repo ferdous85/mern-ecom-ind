06454ab4f6340adab8e89f6fa0b7871000ac799a
ferdous85 <ferdous85@gmail.com>
2021-10-17 18:48:25 +0600

    pagination added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,7 +19,7 @@ exports.createProduct = catchAsyncError(
 // Get All Products -- Admin
 exports.getAllProducts = catchAsyncError(async (req, res, next)=>{
 
-    const resultPerPage= 18
+    const resultPerPage= 8
     const productsCount = await Product.countDocuments()
     const apiFeature = new ApiFeatures(Product.find(), req.query)
     .search()
@@ -31,7 +31,8 @@ exports.getAllProducts = catchAsyncError(async (req, res, next)=>{
     res.status(200).json({
         success:true,
         products,
-        productsCount
+        productsCount,
+        resultPerPage
     })
 })
 
--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -1,22 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Products.css'
 import { clearErrors, getProduct } from '../../actions/productAction'
 import {useSelector, useDispatch} from 'react-redux'
 import Loader from '../layout/Loader/Loader'
 import ProductCard from '../Home/ProductCard'
 import Pagination from 'react-js-pagination'
+import {useAlert} from 'react-alert'
 
 const Products = ({match}) => {
     const dispatch= useDispatch()
-    const {loading, error, products, productsCount} = useSelector((state)=> state.products)
+    const alert = useAlert()
+    const [currentPage, setCurrentPage] = useState(1);
+    const {loading, error, products, productsCount, resultPerPage} = useSelector((state)=> state.products)
     const keyword = match.params.keyword
+    const setCurrentPageNo = (e) => {
+        setCurrentPage(e);
+      };
+
     useEffect(() => {
         if(error) {
             alert.error(error)
             dispatch(clearErrors())
         }
-        dispatch(getProduct(keyword))
-    }, [dispatch, error, keyword])
+        dispatch(getProduct(keyword, currentPage))
+    }, [dispatch, error, keyword,currentPage, alert])
 
     return (
         <>
@@ -30,6 +37,23 @@ const Products = ({match}) => {
                     <ProductCard product={product} />
                 ))}
                 </div>
+
+                {resultPerPage < productsCount && <div className="paginationBox">
+                <Pagination
+                    activePage={currentPage}
+                    itemsCountPerPage={resultPerPage}
+                    totalItemsCount={productsCount}
+                    onChange={setCurrentPageNo}
+                    nextPageText="Next"
+                    prevPageText="Prev"
+                    firstPageText="1st"
+                    lastPageText="Last"
+                    itemClass="page-item"
+                    linkClass="page-link"
+                    activeClass="pageItemActive"
+                    activeLinkClass="pageLinkActive"
+                />
+            </div>}
                 </>
             ) }
         </>
